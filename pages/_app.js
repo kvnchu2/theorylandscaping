a6a98d6354fc6820325075bd5b69ba747620ec9a
Kevin Chu <kvnchu2@gmail.com>
2022-12-07 14:39:12 -0800

    add about page content

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar";
 import "../styles/globals.css";
 import "../styles/home.modules.css";
 import "../styles/root.modules.css";
+import "../styles/about.modules.css";
 
 function MyApp({ Component, pageProps }) {
   return (
